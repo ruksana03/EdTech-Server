e72fa2f442bc2e1e53de474b809cac2a25a1f762
nayon117 <hasibul.nayon1@gmail.com>
2024-02-01 14:50:18 +0600

    add notice apis

---
+++ src/routes/v1/notices/index.js
@@ -0,0 +1,14 @@
+var express = require("express");
+const { findOneNotice, findAllNotices, createNotice, findNoticeQuery, findNoticeUser, updateNotice, deleteNotice } = require("../../../api/v1/notices/controller.js");
+var router = express.Router()
+
+
+router.post('/notices', createNotice )
+router.get("/notices", findAllNotices)
+router.get('/notice/:id', findOneNotice)
+router.get("/notices-query", findNoticeQuery);
+router.get('/notice-user', findNoticeUser)
+router.put('/notice-updated/:id', updateNotice);
+router.delete('/notice/:id',deleteNotice );
+
+module.exports = router;
