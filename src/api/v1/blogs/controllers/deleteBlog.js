e0fbee3e9b79dece25d674b5fb1dbbe62e92320b
Md-ShakilHossain <shakilh550@gmail.com>
2024-02-07 23:04:50 +0600

    added delete api for blogs

--- src/api/v1/blogs/controllers/deleteBlog.js
+++ src/api/v1/blogs/controllers/deleteBlog.js
@@ -3,8 +3,7 @@ const Blog = require("../../../../models/Blogs");
 const deleteBlog = async (req, res) => {
     try {
         const id = req.params.id;
-        console.log("From blog",id);
-        const query = { _id: new ObjectId(id) };
+        const query = { _id: id };
         const result = await Blog.deleteOne(query);
         res.send(result);
     } catch (error) {
