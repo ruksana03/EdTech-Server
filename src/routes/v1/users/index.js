06dbbd9035e1aed7f7a9591433c4a3c91b772e92
Sushil Hemrom <sushilhemrom98@gmail.com>
2024-02-08 19:56:44 +0600

    updated new

--- src/routes/v1/users/index.js
+++ src/routes/v1/users/index.js
@@ -3,7 +3,8 @@ const {
   createUser,
   findAllUser,
   findOneUser,
-  patchUserToChangeRole
+  patchUserToChangeRole,
+  findRole
 } = require("../../../api/v1/users/controllers.js");
 
 var router = express.Router();
@@ -11,6 +12,7 @@ var router = express.Router();
 router.post("/users", createUser);
 router.get("/users", findAllUser);
 router.get("/users/:id", findOneUser);
+router.get("/user", findRole);
 router.patch('/users/:id', patchUserToChangeRole);
 
 module.exports = router;
