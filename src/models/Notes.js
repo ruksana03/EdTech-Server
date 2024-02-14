231f0c317a5eef3dc1b150b2b3a4d19bb3f6df5d
nayon117 <hasibul.nayon1@gmail.com>
2024-02-14 11:02:00 +0600

    create add to cart apis

--- src/models/Notes.js
+++ src/models/Notes.js
@@ -5,19 +5,25 @@ const noteSchema = new mongoose.Schema({
     type: String,
     required: true
   },
-  deadline: {
-    type: Date,
+  description: {
+    type: String,
     required: true
   },
-  email: {
+  priority: {
     type: String,
+    enum: ['low', 'moderate', 'high'],
+    required: true
+  },
+  deadline: {
+    type: Date,
     required: true
   },
   status: {
     type: String,
     enum: ['pending', 'in-progress', 'completed'],
-    default: 'pending'
+     
   }
 });
 
-module.exports = mongoose.model('Note', noteSchema);
+const Note = mongoose.model('Note', noteSchema);
+module.exports = Note;
